268970c463f786b6eebf1a8a67f1c3bbdaeddf27
Louis <louis.peizerat@etu.univ-lyon1.fr>
2022-02-07 11:10:46 +0100

    fix for not enough values

--- api/utils.py
+++ api/utils.py
@@ -5,7 +5,7 @@ def get_eCO2_link_last_24h():
     to = datetime.now()
     start = to - timedelta(hours=23, minutes=59, seconds=59)
     return "https://opendata.reseaux-energies.fr/api/records/1.0/search/?dataset=eco2mix-regional-tr&q=date_heure%3A%5B{}"\
-        "Z+TO+{}Z%5D&rows=1152&sort=-date_heure&facet=libelle_region&facet=nature&facet=date_heure".format(
+        "Z+TO+{}Z%5D&rows=10000&sort=-date_heure&facet=libelle_region&facet=nature&facet=date_heure".format(
             start.strftime("%Y-%m-%dT%H:%M:%S"), to.strftime("%Y-%m-%dT%H:%M:%S"))
 
 
