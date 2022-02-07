cfd19c41711b343195664bc5ba0c23b9f31f1ec1
Joachim <joachim.piq@gmail.com>
2022-02-07 23:40:10 +0100

    Ajout pieChart et layout pour les graphs, changement dossier composant

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-import Map from "./modules/Map";
-import LineChart24hEco from "./modules/LineChart24hEco";
+import Map from "./components/Map";
+import LineChart24hEco from "./components/LineChart24hEco";
 import "./App.css";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import LineChartWeek from "./components/LineChartWeek";
+import PieChartSourceEnergie from "./components/PieChartSourceEnergie";
 
 class App extends Component {
   constructor(props) {
@@ -27,7 +29,15 @@ class App extends Component {
             <Map />
           </Grid>
           <Grid item xs={6} md={6}>
-            <LineChart24hEco />
+            <Grid item xs={5}>
+              <LineChartWeek />
+            </Grid>
+            <Grid item xs={5}>
+              <LineChart24hEco />
+            </Grid>
+            <Grid item xs={5}>
+              <PieChartSourceEnergie />
+            </Grid>
           </Grid>
         </Grid>
       </Box>
