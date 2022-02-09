040dd043b9090af9f33c71491b4b9cac8f17b034
Joachim <joachim.piq@gmail.com>
2022-02-09 15:19:33 +0100

    -Ajout de la page de login (pas branché)

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,14 @@
 import React, { useState } from "react";
-import Map from "./components/Map";
-import AreaChartEcoSource from "./components/AreaChartEcoSource";
 import "./App.css";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import LineCharteEcoConso from "./components/LineCharteEcoConso";
-import PieChartSourceEnergie from "./components/PieChartSourceEnergie";
-import { styled } from "@mui/material/styles";
-import Paper from "@mui/material/Paper";
-import ButtonAppBar from "./components/ButtonAppBar";
+import DashBoard from "./components/Dashboard";
+import Login from "./components/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [selectedRegion, updateRegion] = useState("all");
-  const updateRegionFormat = (region) => updateRegion(region.toLowerCase());
-
-  const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  }));
-
-  return (
-    <Box>
-      <Box sx={{ margin: 5 }}>
-        <ButtonAppBar />
-      </Box>
-      <Box sx={{ flexGrow: 1, margin: 5 }}>
-        <Grid container spacing={10}>
-          <Grid item xs={12} md={6}>
-            <Item>
-              <Map updateRegion={updateRegionFormat} />
-            </Item>
-          </Grid>
-          <Grid item xs={12} md={6} container spacing={5}>
-            <Grid item xs={12} md={12} xl={6}>
-              <Item>
-                <LineCharteEcoConso selectedRegion={selectedRegion} />
-              </Item>
-            </Grid>
-            <Grid item xs={12} md={12} xl={6}>
-              <Item>
-                <AreaChartEcoSource selectedRegion={selectedRegion} />
-              </Item>
-            </Grid>
-            <Grid item xs={12} md={12} xl={6}>
-              <Item>
-                <PieChartSourceEnergie selectedRegion={selectedRegion} />
-              </Item>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Box>
-    </Box>
-  );
+  const [token, setToken] = useState();
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+  return <DashBoard />;
 }
-
 export default App;
